093c0068c3dfc5010aac603ca099a98d3f7d652b
ShibataJose <jose_shibata@hotmail.com>
2022-08-19 21:44:01 -0300

    Aula Comentada

--- index.php
+++ index.php
@@ -4,8 +4,66 @@
     Comentários em Bloco
     */
 
-    /*adfkhfguskaf*/
+   /* variáveis e condicionais
+   
+   $nome = "Zé";
 
-    echo "Olá mundo";
+    CONST IDADE = 10;
+
+    echo "Olá mundo, Seja bem vindo ", $nome, " minha idade é : ", IDADE, "<br> <br>";
+
+    if(10>12){
+        echo "Verdadeiro";
+    }else{
+        echo "falso <br>";
+    }*/
+
+    //faca um if que calcule a média e escreva na tela se foi arpovado ou não
+
+   /* $nota1 = 8;
+    $nota2 = 6;
+    $nota3 = 5;
+
+    echo "<br>";
+
+    if(($nota1+$nota2+$nota3)/3>=6){
+        echo "Você foi aprovado";
+    }else{
+        echo "<br> Você foi reprovado";
+    }*/
+
+    //loopin
+
+/* Laços de repetição
+
+    $contadora = 0;
+
+    do{
+
+        $contadora++;
+        //executa os códigos
+
+    }while(10<$contadora);
+
+    $count = 0;
+
+    while($count < 10){
+        $cont++;
+        //executa os códigos
+    }
+
+    for($i = 0; $i<10; i++){
+        //executa os códigos
+    }
+*/
+
+/*imprimir todos os numeros pares entre 1 e 545199
+
+for($i = 1; $i<545199; $i++){
+    if($i % 2 == 0){
+        echo "<br>",$i;
+    }
+}*/
 
 ?>
+
